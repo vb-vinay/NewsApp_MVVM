bb2f938d2ba5b09fb069f9cc4c07b04fad2d9537
Vinay Bharwani <bharwani.vinay@gmail.com>
2019-12-10 15:47:22 +0530

    Initial Commit

---
+++ NewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by Vinay Bharwani on 10/12/19.
+//  Copyright © 2019 Vinay Bharwani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
